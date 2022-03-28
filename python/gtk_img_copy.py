20d11b738c19fd56a7f811f6558942ead5b09a70
home <seotk79@gmail.com>
2022-03-28 22:41:45 +0900

    add: Gtk.FileChooserDialog

--- python/gtk_img_copy.py
+++ python/gtk_img_copy.py
@@ -13,6 +13,22 @@ gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, GLib, GObject, Gio, Adw
 
+# https://github.com/natorsc/gui-python-gtk/blob/main/src/gtk4/file-chooser-dialog-folder/MainWindow.py
+class SelectDir(Gtk.FileChooserDialog):
+    def __init__(self, title, widget, parent):
+        Gtk.FileChooserDialog.__init__(self)
+        self.set_action(action=Gtk.FileChooserAction.SELECT_FOLDER)
+        self.label = widget.get_label()
+        self.set_title(title)
+        self.set_transient_for(parent)
+        self.set_default_size(600, 400)
+        self.set_modal(True)
+        self.add_buttons(
+            "Cancel", Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+        )
+    def get_label(self):
+        return self.label
+
 class Window(Gtk.ApplicationWindow):
     def __init__(self, title, width, height, **kwargs):
         super(Window, self).__init__(**kwargs)
@@ -27,8 +43,8 @@ class Window(Gtk.ApplicationWindow):
         # dictionary
         copy_info = {}
         
-        copy_info["text_in_path"] = self.text_in_path.get_text()
-        copy_info["text_out_path"] = self.text_out_path.get_text()
+        copy_info["lb_in_path"] = self.lb_in_path.get_text()
+        copy_info["lb_out_path"] = self.lb_out_path.get_text()
         
         self.thread_running = True
         thread = threading.Thread(target=self.on_thread_working, args=(copy_info, ))
@@ -41,7 +57,7 @@ class Window(Gtk.ApplicationWindow):
         self.lb_ing_count.set_text(str(args['copy_count']) + " / " + str(args['all_count']))
 
     def on_thread_working(self, copy_info):
-        in_file_list = get_dir_list(copy_info["text_in_path"])
+        in_file_list = get_dir_list(copy_info["lb_in_path"])
         
         all_count = len(in_file_list)
         copy_count = 0
@@ -57,9 +73,9 @@ class Window(Gtk.ApplicationWindow):
                 continue
 
             #str_date = n_date.strftime("%Y %m %d %H:%M:%S")
-            #tmp_path = self.text_out_path.get_text() + "/" + n_date.strftime("%Y/%m/%d/")
+            #tmp_path = self.lb_out_path.get_text() + "/" + n_date.strftime("%Y/%m/%d/")
             # 2020/2020년8월
-            tmp_path = copy_info["text_out_path"] + "/" + n_date.strftime("%Y/%Y년%m월/")
+            tmp_path = copy_info["lb_out_path"] + "/" + n_date.strftime("%Y/%Y년%m월/")
             if not os.path.isdir(tmp_path):
                 os.makedirs(tmp_path)
 
@@ -68,7 +84,7 @@ class Window(Gtk.ApplicationWindow):
                 continue
 
             #print("copy => " + n_file)
-            file_copy(copy_info["text_in_path"] + "/" + n_file, tmp_path + n_file)
+            file_copy(copy_info["lb_in_path"] + "/" + n_file, tmp_path + n_file)
             copy_count = copy_count + 1
             GLib.idle_add(self.on_update_status, {'copy_count':copy_count, 'all_count':all_count, 'n_file':n_file})
             # time.sleep(0.2)
@@ -93,10 +109,10 @@ class Window(Gtk.ApplicationWindow):
             print("Folder selected: " + dialog.get_filename())
             print("btn label: " + widget.get_label())
             if widget.get_label() == "Input(DIR)":
-                self.text_in_path.set_text(dialog.get_filename())
+                self.lb_in_path.set_text(dialog.get_filename())
                 print("input label")
             elif widget.get_label() == "Output(DIR)":
-                self.text_out_path.set_text(dialog.get_filename())
+                self.lb_out_path.set_text(dialog.get_filename())
                 print("output label")
 
         elif response == Gtk.ResponseType.CANCEL:
@@ -154,14 +170,14 @@ class MyWindow(Window):
         content.append(label)
         content.append(content_sub1)
 
-        lb_in_path = Gtk.Entry()
-        lb_out_path = Gtk.Entry()
-        lb_in_path.set_text(self.in_path)
-        lb_out_path.set_text(self.out_path)
-        lb_in_path.set_halign(Gtk.Align.START)
-        lb_out_path.set_halign(Gtk.Align.START)
-        content2_l.append(lb_in_path)
-        content2_r.append(lb_out_path)
+        self.lb_in_path = Gtk.Entry()
+        self.lb_out_path = Gtk.Entry()
+        self.lb_in_path.set_text(self.in_path)
+        self.lb_out_path.set_text(self.out_path)
+        self.lb_in_path.set_halign(Gtk.Align.START)
+        self.lb_out_path.set_halign(Gtk.Align.START)
+        content2_l.append(self.lb_in_path)
+        content2_r.append(self.lb_out_path)
         
         button = Gtk.Button()
         button.set_label('in path')
@@ -175,46 +191,53 @@ class MyWindow(Window):
         button.connect('clicked', self.on_dir_clicked)
         content2_r.append(button)
 
-        lb_result = Gtk.Label()
-        lb_result.set_text('copy : 0/0')
-        content.append(lb_result)
+        self.lb_process = Gtk.Label()
+        self.lb_process.set_text('')
+        content.append(self.lb_process)
+        self.lb_ing_count = Gtk.Label()
+        self.lb_ing_count.set_text('')
+        content.append(self.lb_ing_count)
 
         button = Gtk.Button()
         button.set_label('Start')
         button.connect("clicked", self.on_start_copy_clicked)
         content.append(button)
- 
         self.thread_running = False
-
         self.set_child(content)
 
     def on_dir_clicked(self, widget):
+        """
         dialog = Gtk.FileChooserDialog(
             title="Please choose a folder",
             action=Gtk.FileChooserAction.SELECT_FOLDER,
         )
-
         dialog.add_buttons(
             "Cancel", Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
         )
         dialog.set_transient_for(self)
-        dialog.set_modal(True)
         dialog.set_default_size(600, 400)
+        dialog.set_modal(True)
+        dialog.connect('response', self.on_dialog_response)
+        dialog.show()
+        """
+        dialog = SelectDir("Please choose a folder", widget, self)
         dialog.connect('response', self.on_dialog_response)
         dialog.show()
         
     def on_dialog_response(self, dialog, response):
         if response == Gtk.ResponseType.OK:
             print("Select clicked")
-            print("Folder selected: " + dialog.get_filename())
+            glocalfile = dialog.get_file()
+            print("Folder selected: " + glocalfile.get_basename())
+            print("Folder selected: " + glocalfile.get_path())
+            label = dialog.get_label()
+            path = glocalfile.get_path()
             
-            #print("btn label: " + widget.get_label())
-            #if widget.get_label() == "Input(DIR)":
-            #    self.text_in_path.set_text(dialog.get_filename())
-            #    print("input label")
-            #elif widget.get_label() == "Output(DIR)":
-            #    self.text_out_path.set_text(dialog.get_filename())
-            #    print("output label")
+            print("btn label: " + label)
+            if label == "in path":
+                self.lb_in_path.set_text(path)
+            elif label == "out path":
+                self.lb_out_path.set_text(path)
         elif response == Gtk.ResponseType.CANCEL:
             print("Cancel clicked")
 
